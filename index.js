5e8c8f14cb9269e4c15c68868d7d9212a2f91295
Masavain <matti.vainionpaa@helsinki.fi>
2018-02-10 15:11:51 +0200

    initial

---
+++ index.js
@@ -0,0 +1,27 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+
+// if (process.env.NODE_ENV !== 'production') {
+//     require('dotenv').config()
+//   }
+
+const mongoUrl = 'mongodb://masa:blogipassu123@ds129428.mlab.com:29428/blogilista-production-db'
+// const mongoUrl =  process.env.MONGODB_URI
+mongoose.connect(mongoUrl)
+mongoose.Promise = global.Promise
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.static('build'))
+
+app.use('/api/blogs', blogsRouter)
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
